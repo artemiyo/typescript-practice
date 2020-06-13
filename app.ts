2e5db06cd24c0da2f2dba85fbb8377db9324a160
Artem <50479756+artemiyo@users.noreply.github.com>
2020-06-13 10:33:49 +0300

    private, protected and readonly

--- app.ts
+++ app.ts
@@ -1,51 +1,29 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: 'Maximilian',
-//   age: 30,
-//   hobbies: ['Sports', 'Cooking'],
-//   role: [2, 'author']
-// };
+// Protected & Private & Readonly
 
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
+class Department {
+  constructor(
+    private readonly name: string,
+    private employess: string[] = []
+  ) {}
 
-enum Role {
-  ADMIN = "ADMIN",
-  READ_ONLY = 100,
-  AUTHOR = "AUTHOR",
+  describe() {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employess.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employess.length);
+    console.log(this.employess);
+  }
 }
 
-const person = {
-  name: "Maximilian",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  role: Role.ADMIN,
-};
+const accounting = new Department("Accounting");
 
-// person.role.push('admin');
-// person.role[1] = 10;
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
 
-// person.role = [0, 'admin', 'user'];
-
-let favoriteActivities: string[];
-favoriteActivities = ["Sports"];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-  // console.log(hobby.map()); // !!! ERROR !!!
-}
-
-if (person.role === Role.AUTHOR) {
-  console.log("is author");
-}
+accounting.describe();
+accounting.printEmployeeInformation();
