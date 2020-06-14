00a3f8fb3366b335a2038e8270f094d026f8b360
Artem <50479756+artemiyo@users.noreply.github.com>
2020-06-14 23:29:03 +0300

    generic types

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,61 @@
-const userInputElement = document.getElementById("user-input");
-
-if (userInputElement) {
-  (userInputElement as HTMLInputElement).value = "Hi there!";
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
 }
 
-// Index properties
-interface ErrorContainer {
-  [prop: string]: string;
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+
+interface Lengthy {
+  length: number;
 }
 
-const errorBag: ErrorContainer = {
-  email: "Not a valid email!",
-  username: "Must start with the capital character",
-};
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element";
+  } else if (element.length) {
+    descriptionText = "Got " + element.length + " elements";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
+}
+
+console.log(extractAndConvert({ name: "Max" }, "name"));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) !== -1) {
+      this.data.splice(this.data.indexOf(item), 1);
+    }
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Max");
+console.log(textStorage.getItems());
+
+// const objStorage = new DataStorage<object>();
+// const maxObj = { name: "Max" };
+// objStorage.addItem(maxObj);
+// objStorage.addItem({ name: "Manu" });
+// objStorage.removeItem(maxObj);
+// console.log(objStorage.getItems());
